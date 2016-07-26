4437ab09214d3a1ffd7af5f4bf9281a76fbe03c6
Ander Conal <ander.conal@gmail.com>
2016-07-26 19:56:42 +0200

    Setting bean properties

--- src/main/java/es/anderconal/modelo/Persona.java
+++ src/main/java/es/anderconal/modelo/Persona.java
@@ -3,6 +3,7 @@ package es.anderconal.modelo;
 public class Persona {
 	private int id;
 	private String nombre;
+	private int taxId;
 	
 	public Persona() {
 
@@ -13,12 +14,16 @@ public class Persona {
 		this.nombre = nombre;
 	}
 
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
+
 	public void hablar() {
 		System.out.println("Hola, soy una persona");
 	}
 	
 	@Override
 	public String toString() {
-		return "Persona [id=" + id + ", nombre=" + nombre + "]";
+		return "Persona [id=" + id + ", nombre=" + nombre + ", taxId=" + taxId +"]";
 	}
 }
