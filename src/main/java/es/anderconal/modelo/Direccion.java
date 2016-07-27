136cfdc110f2320ca176c86725c570f08d64ae08
Ander Conal <ander.conal@gmail.com>
2016-07-27 19:06:37 +0200

    Using P namespaces
    Alternative for setting bean properties

--- src/main/java/es/anderconal/modelo/Direccion.java
+++ src/main/java/es/anderconal/modelo/Direccion.java
@@ -4,6 +4,10 @@ public class Direccion {
 	private String calle;
 	private String codigoPostal;
 	
+	public Direccion() {
+		
+	}
+	
 	public Direccion(String calle, String codigoPostal) {
 		super();
 		this.calle = calle;
@@ -12,6 +16,22 @@ public class Direccion {
 	
 	public void iniciar() {
 		System.out.println("Bean creado: " + this);
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
 	}
 
 	@Override
--- src/main/java/es/anderconal/programa/Programa.java
+++ src/main/java/es/anderconal/programa/Programa.java
@@ -11,20 +11,8 @@ public class Programa {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("es/anderconal/beans/beans.xml");
 		
-		Persona persona = (Persona) context.getBean("persona");
-		persona.hablar();
-		
-		Persona personaDos = (Persona) context.getBean("personaDos");
-		personaDos.hablar();
-		persona.setTaxId(456);
-		
-		System.out.println(persona);
-		System.out.println(personaDos);
-		
-		Direccion direccion = (Direccion) context.getBean("direccion");
-		System.out.println(direccion);
-		
-		System.out.println(persona);
+		Direccion direccionDos = (Direccion) context.getBean("direccionDos");
+		System.out.println(direccionDos);
 		
 		((ClassPathXmlApplicationContext)context).close();
 
